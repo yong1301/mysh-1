bdc32542577d0efcce858a80882056c4e6d5b919
yong1301 <yong1301@ajou.ac.kr>
2017-11-05 01:21:19 +0900

    process creation completion

--- src/commands.c
+++ src/commands.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "commands.h"
 #include "built_in.h"
@@ -32,8 +36,16 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
   if (n_commands > 0) {
     struct single_command* com = (*commands);
-
+    struct stat buf;
+    int status;
+    char *env_path;
+	
     assert(com->argc != 0);
+
+    stat(com->argv[0], &buf);
+	env_path = getenv("PATH");
+
+	printf("PATH = %s\n", env_path);
 
     int built_in_pos = is_built_in_command(com->argv[0]);
     if (built_in_pos != -1) {
@@ -49,7 +61,18 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
       return 0;
     } else if (strcmp(com->argv[0], "exit") == 0) {
       return 1;
-    } else {
+    } else if (S_ISREG(buf.st_mode)) {
+      pid_t pid;
+      
+      pid = fork();
+	  if(pid == 0){
+      execv(com->argv[0], com->argv);
+      }
+      else if(pid > 0){
+      wait(&status);
+      }      
+    }
+	else {
       fprintf(stderr, "%s: command not found\n", com->argv[0]);
       return -1;
     }
