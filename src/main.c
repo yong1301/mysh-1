c455ba43b6e1ed617d7d25494d9bdaf69a67bf1d
yong1301 <yong1301@ajou.ac.kr>
2017-11-08 19:34:05 +0900

    signal_handlers completion

--- src/main.c
+++ src/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
+#include "signal_handlers.h"
 
 int main()
 {
   char buf[8096];
+  signal(SIGINT, catch_sigint);
+  signal(SIGTSTP, catch_sigtstp);
 
   while (1) {
-    fgets(buf, 8096, stdin);
+	fgets(buf, 8096, stdin);
 
     struct single_command commands[512];
     int n_commands = 0;
@@ -21,6 +25,8 @@ int main()
 
     free_commands(n_commands, &commands);
     n_commands = 0;
+
+	memset(buf, '\0', 8096);
 
     if (ret == 1) {
       break;
--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,17 @@
 #include "signal_handlers.h"
+#include <signal.h>
+#include <stdio.h>
 
 void catch_sigint(int signalNo)
 {
-  // TODO: File this!
+  printf("\nYour input Ctrl+C\n");
+  printf("Shell doesn't close.\n");
+  signal(SIGINT, catch_sigint);
 }
+
 
 void catch_sigtstp(int signalNo)
 {
-  // TODO: File this!
+  printf("\nYour input Ctrl+Z, but it doesn't move.\n");
+  signal(SIGTSTP, catch_sigtstp);
 }
